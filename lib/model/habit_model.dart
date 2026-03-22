2778e08bc594e096ecaa44ec0afdf8718237313b
Nancy Quan <51468095+Yaoink@users.noreply.github.com>
2026-03-22 08:07:43 -0400

    feat: create habit model

---
+++ lib/model/habit_model.dart
@@ -0,0 +1,46 @@
+class HabitModel {
+  int? id;
+  String name;
+  String description;
+  String rewardReq;
+  String penaltyReq;
+  String frequency;
+  int streak;
+
+  HabitModel({
+    this.id,
+    required this.name,
+    required this.description,
+    required this.rewardReq,
+    required this.penaltyReq,
+    required this.frequency,
+    this.streak = 0,
+  });
+
+  // Convert HabitModel to Map for database storage
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'rewardReq': rewardReq,
+      'penaltyReq': penaltyReq,
+      'frequency': frequency,
+      'streak': streak,
+    };
+  }
+
+  // Create HabitModel from Map retrieved from database
+  factory HabitModel.fromMap(Map<String, dynamic> map) {
+    return HabitModel(
+      id: map['id'],
+      name: map['name'],
+      description: map['description'],
+      rewardReq: map['rewardReq'],
+      penaltyReq: map['penaltyReq'],
+      frequency: map['frequency'],
+      streak: map['streak'],
+    );
+  }
+}
+
