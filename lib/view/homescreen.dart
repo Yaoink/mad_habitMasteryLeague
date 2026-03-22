20e4e9be5f1d493134d8f4644c3564a319a53e13
Nancy Quan <51468095+Yaoink@users.noreply.github.com>
2026-03-22 15:06:00 -0400

    feat:create view to take user input from habit from

---
+++ lib/view/homescreen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:project1/model/database_helper.dart';
+import 'package:project1/model/habit_model.dart';
+import 'package:project1/view/add_habit.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<HabitModel> habits = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHabits();
+  }
+
+  Future<void> _loadHabits() async {
+    final dbHelper = DatabaseHelper.instance;
+    final habitMaps = await dbHelper.getAllItems();
+    setState(() {
+      habits = habitMaps.map((map) => HabitModel.fromMap(map)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Habit Tracker'),
+      ),
+      body: ListView.builder(
+        itemCount: habits.length,
+        itemBuilder: (context, index) {
+          final habit = habits[index];
+          return ListTile(
+            title: Text(habit.name),
+            subtitle: Text(habit.description),
+            trailing: Text('Streak: ${habit.streak}'),
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddHabit()),
+          ).then((_) => _loadHabits()); // Refresh habits after adding new one
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
